63dcda2bc10750a451beec25a63bde11db517f53
Jason <jason.zou@gmail.com>
2024-12-02 13:10:00 -0500

    alias @ and vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import UnoCSS from 'unocss/vite'
+import { defineConfig, loadEnv } from 'vite'
+import alias from './vite/alias'
+import setupVitePlugins from './vite/plugins'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    UnoCSS(),
-  ],
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    plugins: setupVitePlugins(env, command === 'build'),
+    resolve: {
+      alias
+    }
+  }
 })
---
+++ vite/plugins/index.ts
@@ -0,0 +1,28 @@
+import type { PluginOption } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import UnoCSS from 'unocss/vite'
+// import vueJsx from '@vitejs/plugin-vue-jsx'
+// import setupAutoImport from './auto-import'
+// import setupComponents from './components'
+// import setupSvgIcon from './svg-icon'
+// import setupIcons from './icon'
+// import setupMock from './mock'
+// import setupCompression from './compression'
+
+export default function setupVitePlugins(viteEnv: Record<string, string>, isBuild: boolean) {
+  const { VITE_USE_MOCK, VITE_BUILD_COMPRESS } = viteEnv
+  const plugins: PluginOption[] =[vue(), UnoCSS()]
+
+//   const plugins: PluginOption[] = [vue(), vueJsx(), UnoCSS()]
+//   plugins.push(setupAutoImport())
+//   plugins.push(setupComponents())
+//   plugins.push(setupIcons())
+//   plugins.push(setupSvgIcon(isBuild))
+//   if (isBuild) {
+//     const compressList = VITE_BUILD_COMPRESS.split(',')
+//     if (compressList.includes('gzip') || compressList.includes('brotli'))
+//       plugins.push(...setupCompression(viteEnv))
+//   }
+//   VITE_USE_MOCK === 'true' && plugins.push(setupMock(isBuild))
+  return plugins
+}
